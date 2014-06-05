92fb4ef0b05e6ca364acf11c4e93d011422ad5dc
Gavin King <gavin.king@fronter.com>
2014-06-05 16:12:02 +0200

    ADded localstorage for dev convenience

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ angular.module('game', [])
 
         // Create your first card.
         $scope.myCard = $scope.makeCard();
+        $scope.myGo = false;
     })
 
     .directive('cards', function () {
@@ -32,8 +33,13 @@ angular.module('game', [])
 
             templateUrl: 'connection-interface.html',
 
-            controller: function ($scope) {
-                var bindConnection = function (conn) {
+            controller: function ($scope, $window) {
+                var retrieveStoredData = function () {
+                    $scope.peerId = $window.localStorage.getItem('opponentId');
+                    $scope.myId = $window.localStorage.getItem('gameId');
+                },
+
+                bindConnection = function (conn) {
                         $scope.conn = conn;
                         $scope.connected = true;
 
@@ -51,20 +57,27 @@ angular.module('game', [])
                         });
                     },
 
-                    createPeer = function () {
-                        // This is you
-                        $scope.peer = new Peer({key: '6cvuigpzcc0ltyb9'});
+                    createPeer = function (id) {
+                        retrieveStoredData();
+
+                        // This is your peer object which controls all your connections
+                        $scope.peer = $scope.myId ?
+                            new Peer($scope.myId, {key: '6cvuigpzcc0ltyb9'}) :
+                            new Peer({key: '6cvuigpzcc0ltyb9'});
 
                         // This is when you have registered with the peerjs server
-                        // All users will run this code and become peers.
                         $scope.peer.on('open', function (id) {
                             $scope.status = 'My peer ID is: ' + id;
                             $scope.myId = id;
 
+                            // save it for later
+                            $window.localStorage.setItem('gameId', id);
+
                             $scope.$apply();
                         });
 
-                        // When someone else connects to you, the connection event is thrown.
+                        // When someone else initialises a connection to you,
+                        // the 'connection' event is thrown on the 'peer' object
                         $scope.peer.on('connection', function (conn) {
                             bindConnection(conn);
 
@@ -76,7 +89,12 @@ angular.module('game', [])
                 // Click connect button and fire this
                 $scope.connect = function (peerId) {
                     var conn = $scope.peer.connect(peerId);
+
+                    // Store it for convenience
+                    $window.localStorage.setItem('opponentId', peerId);
+
                     $scope.status = 'Connecting to ' + peerId;
+                    $scope.myGo = true;
                     bindConnection(conn);
                 };
 
@@ -105,4 +123,13 @@ angular.module('game', [])
                 });
             }
         };
-    });
+    })
+    .directive('card', function () {
+        return {
+            restrict: 'E',
+            scope: {
+                data: '='
+            },
+            templateUrl: 'card.html'
+        };
+    })
