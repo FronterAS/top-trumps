2cf5c037d2700e7454c3964e87822b8b2dde63f2
Gavin King <gavin.king@fronter.com>
2014-06-05 14:40:54 +0200

    Fiddling

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 angular.module('game', [])
+
     .controller('MainController', function ($scope) {
         var makeNumber = function () {
                 return Math.floor(Math.random() * 10);
@@ -15,6 +16,7 @@ angular.module('game', [])
         // This is you
         $scope.peer = new Peer({key: '6cvuigpzcc0ltyb9'});
         // This is when you have registered with the peerjs server
+        // All users will run this code and become peers.
         $scope.peer.on('open', function (id) {
             $scope.status = 'My peer ID is: ' + id;
             $scope.myId = id;
@@ -22,6 +24,7 @@ angular.module('game', [])
             $scope.$apply();
         });
 
+        // Create your first card.
         $scope.myCard = $scope.makeCard();
     })
 
@@ -41,35 +44,29 @@ angular.module('game', [])
             templateUrl: 'gameboard.html',
 
             controller: function ($scope) {
-                var setupConnData = function () {
+                var bindConnection = function (conn) {
+                        $scope.conn = conn;
+                        $scope.connected = true;
+
+                        // When data is receieved
                         $scope.conn.on('data', function (data) {
                             $scope.status = 'Ta da, a message came in!';
                             $scope.yourCard = data;
+                            $scope.$apply();
+                        });
+
+                        // When the connection is opened...
+                        $scope.conn.on('open', function () {
+                            $scope.status = 'Connected to ' + peerId;
                             $scope.$apply();
                         });
                     };
 
                 // Click connect button and fire this
                 $scope.connect = function (peerId) {
+                    var conn = $scope.peer.connect(peerId);
                     $scope.status = 'Connecting to ' + peerId;
-
-                    // Connect to a person
-                    $scope.conn = $scope.peer.connect(peerId);
-
-                    if ($scope.conn) {
-                        $scope.connected = true;
-
-                        // Outgoing messages TO someone
-                        $scope.conn.on('open', function () {
-                            $scope.status = 'Connected to ' + peerId;
-                            $scope.$apply();
-                        });
-
-                        setupConnData();
-
-                    } else {
-                        $scope.status = 'Your connection suffered a case of being totally shit.';
-                    }
+                    bindConnection(conn);
                 };
 
                 // Send the card!
@@ -83,23 +80,17 @@ angular.module('game', [])
                     $scope.conn.send($scope.myCard);
                 };
 
-                // Incoming messages FROM someone
+                // When someone else connects to you, the connection event is thrown.
                 $scope.peer.on('connection', function (conn) {
-
-                    // This will only work for a one on one game
-                    if (!$scope.conn) {
-                        $scope.conn = conn;
-                        setupConnData();
-                    }
+                    bindConnection(conn);
 
                     $scope.status = 'Someone connected to you!';
-                    $scope.connected = true;
-
                     $scope.$apply();
                 });
             },
 
             link: function (scope) {
+                // Use ($)scope.status to print to console log and add a message to the screen.
                 scope.$watch('status', function (value) {
                     if (value !== undefined) {
                         console.log(value);
